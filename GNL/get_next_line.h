79ec27d9bbbe5d59c82ef867ee595236dc4634b9
PACMAN <pacman@codershigh.com>
2021-03-09 15:25:01 +0900

    minor

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -3,11 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pac-man <pac-man@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 18:09:19 by taeskim           #+#    #+#             */
-/*   Updated: 2021/03/08 18:09:51 by taeskim          ###   ########.fr       */
+/*   Updated: 2021/03/09 15:23:52 by pac-man          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int get_next_line(int fd, char **line);
