dd14725944fdec6f0837ed126d736f40b1bb59a7
pacman-x <sr9872@naver.com>
2021-05-08 18:32:12 +0900

    comment removed

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 18:26:05 by taeskim           #+#    #+#             */
-/*   Updated: 2021/05/08 18:26:25 by taeskim          ###   ########.fr       */
+/*   Updated: 2021/05/08 18:31:45 by taeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,6 @@
 
 int get_next_line(int fd, char **line)
 {
-
-	char *buf;
-	static char *backup;
-
-	int state;
-
-	buf = (char *)malloc(BUFFER_SIZE + 1);
-	backup = (char *)malloc(OPEN_MAX);
-
-	while ((state = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[state] = '\0';
-		*line = buf;
-
-		printf("this is the read_value: %s \n", buf);
-	}
 
 	return (0);
 }
