7e8c9f5776e56c2e2de63fd6189e89daf011e843
pacman-x <sr9872@naver.com>
2021-05-19 02:52:45 +0900

    first try

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 18:26:05 by taeskim           #+#    #+#             */
-/*   Updated: 2021/05/19 00:01:53 by taeskim          ###   ########.fr       */
+/*   Updated: 2021/05/19 02:50:12 by taeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,27 @@ void			*replace(char **out, char *src)
 	return (*out);
 }
 
+int				back_up_to_line(char **line, char **back_up, size_t offset)
+{
+	int index;
+	int len;
+
+	index = -1;
+	len = ft_strlen(*back_up) - offset;
+	*line = (char *)malloc(offset + 1);
+	(*line)[offset] = 0;
+	while ((*back_up)[++index] != '\n')
+		(*line)[index] = (*back_up)[index];
+	ft_strlcpy(*back_up, *back_up + offset + 1, len);
+	return (1);
+}
+
 int				get_next_line(int fd, char **line)
 {
 	static char	*back_up[OPEN_MAX];
 	char		buff[BUFFER_SIZE + 1];
 	int			read_size;
 	size_t		offset;
-	size_t		index;
 
 	if (!line || read(fd, buff, 0) || BUFFER_SIZE < 1)
 		return (-1);
@@ -36,15 +50,7 @@ int				get_next_line(int fd, char **line)
 	while (back_up[fd][++offset])
 	{
 		if (back_up[fd][offset] == '\n')
-		{
-			index = -1;
-			*line = (char *)malloc(offset + 1);
-			(*line)[offset] = 0;
-			while (back_up[fd][++index] != '\n')
-				(*line)[index] = back_up[fd][index];
-			ft_strlcpy(back_up[fd], back_up[fd] + offset + 1, ft_strlen(back_up[fd]) - offset);
-			return (1);
-		}
+			return (back_up_to_line(line, &(back_up[fd]), offset));
 	}
 	read_size = read(fd, buff, BUFFER_SIZE);
 	buff[read_size] = 0;
@@ -55,20 +61,7 @@ int				get_next_line(int fd, char **line)
 		while (back_up[fd][++offset] || !read_size)
 		{
 			if (back_up[fd][offset] == '\n' || (!read_size && !back_up[fd][offset]))
-			{
-				index = -1;
-				*line = (char *)malloc(offset + 1);
-				(*line)[offset] = 0;
-				while (back_up[fd][++index] && back_up[fd][index] != '\n')
-					(*line)[index] = back_up[fd][index];
-				ft_strlcpy(back_up[fd], back_up[fd] + offset + 1, ft_strlen(back_up[fd]) - offset);
-				if (read_size == 0)
-				{
-					free(back_up[fd]);
-					back_up[fd] = 0;
-				}
-				return (0 < read_size);
-			}
+				return (read_size == 0 ? 0 : back_up_to_line(line, &(back_up[fd]), offset));
 		}
 		read_size = read(fd, buff, BUFFER_SIZE);
 		buff[read_size] = 0;
